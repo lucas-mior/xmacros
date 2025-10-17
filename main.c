bcbbcda1e23cad28fcc80b35fccde2c2d26649e8
lucas-mior <lucas.mior@tutamail.com>
2025-10-17 15:10:30 -0300

    util.c: multiple fixes and style

--- main.c
+++ main.c
@@ -4,71 +4,61 @@
 #include <stdint.h>
 
 #define STRUCT_NAME NumberStruct
-#define STRUCT_FIELDS \
-    X(char, c) \
-    X(short, s) \
-    X(int, i) \
-    X(long, l) \
-    X(uchar, uc) \
-    X(ushort, us) \
-    X(uint, ui) \
-    X(ulong, ul) \
-    X(float, f) \
+#define STRUCT_FIELDS                                                          \
+    X(char, c)                                                                 \
+    X(short, s)                                                                \
+    X(int, i)                                                                  \
+    X(long, l)                                                                 \
+    X(uchar, uc)                                                               \
+    X(ushort, us)                                                              \
+    X(uint, ui)                                                                \
+    X(ulong, ul)                                                               \
+    X(float, f)                                                                \
     X(double, d)
 #include "fmtgen.h"
 
 #define STRUCT_NAME SmallStruct
-#define STRUCT_FIELDS \
-    X(char *, string) \
+#define STRUCT_FIELDS                                                          \
+    X(char *, string)                                                          \
     X(NumberStruct, number_struct)
 #include "fmtgen.h"
 
 #define STRUCT_NAME BigStruct
-#define STRUCT_FIELDS \
-    X(long, l) \
+#define STRUCT_FIELDS                                                          \
+    X(long, l)                                                                 \
     X(SmallStruct *, small_struct)
 #include "fmtgen.h"
 
-#define EXPAND_STRUCTS \
-    STRUCT(SmallStruct); \
+#define EXPAND_STRUCTS                                                         \
+    STRUCT(SmallStruct);                                                       \
     STRUCT(NumberStruct);
 #include "fmt_functions.h"
 
-#define STRUCT_PRINT(NAME) \
-  _Generic((NAME), \
-    BigStruct *:   struct_print(&BigStruct_fmt,   #NAME, NAME, 0), \
-    NumberStruct *:struct_print(&NumberStruct_fmt,    #NAME, NAME, 0), \
-    SmallStruct *: struct_print(&SmallStruct_fmt, #NAME, NAME, 0) \
-  )
+#define STRUCT_PRINT(NAME)                                                     \
+    _Generic((NAME),                                                           \
+        BigStruct *: struct_print(&BigStruct_fmt, #NAME, NAME, 0),             \
+        NumberStruct *: struct_print(&NumberStruct_fmt, #NAME, NAME, 0),       \
+        SmallStruct *: struct_print(&SmallStruct_fmt, #NAME, NAME, 0))
 
 int
-main(int argc, char **argv)
-{
-    NumberStruct mine = {
-        .c = 'c',
-        .s = SHRT_MAX,
-        .i = INT_MAX,
-        .l = LONG_MAX,
-        .uc = 'd',
-        .us = USHRT_MAX,
-        .ui = UINT_MAX,
-        .ul = ULONG_MAX,
-        .f = 0.5f,
-        .d = 0.5
-    };
-    SmallStruct small = {
-        .string = "superstring",
-        .number_struct = mine
-    };
-    BigStruct big = {
-        .l = 100,
-        .small_struct = &small
-    };
+main(int argc, char **argv) {
+    NumberStruct mine = {.c = 'c',
+                         .s = SHRT_MAX,
+                         .i = INT_MAX,
+                         .l = LONG_MAX,
+                         .uc = 'd',
+                         .us = USHRT_MAX,
+                         .ui = UINT_MAX,
+                         .ul = ULONG_MAX,
+                         .f = 0.5f,
+                         .d = 0.5};
+    SmallStruct small = {.string = "superstring", .number_struct = mine};
+    BigStruct big = {.l = 100, .small_struct = &small};
 
     BigStruct *pbig = &big;
 
-    (void) argc;
-    (void) argv;
+    (void)argc;
+    (void)argv;
 
     STRUCT_PRINT(&small);
     STRUCT_PRINT(&mine);
@@ -95,8 +85,10 @@ main(int argc, char **argv)
         print_buffer(number_struct_buffer, sizeof(small_struct_buffer));
         printf("\n");
 
-        struct_unpack(&SmallStruct_fmt, small_struct_buffer, &small_struct_from_buffer);
-        struct_unpack(&NumberStruct_fmt, number_struct_buffer, &number_struct_from_buffer);
+        struct_unpack(&SmallStruct_fmt, small_struct_buffer,
+                      &small_struct_from_buffer);
+        struct_unpack(&NumberStruct_fmt, number_struct_buffer,
+                      &number_struct_from_buffer);
 
         STRUCT_PRINT(&small_struct_from_buffer);
         STRUCT_PRINT(&number_struct_from_buffer);
