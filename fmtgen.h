811324287f8d20b2ec6bf9aaefb2732171fdb2bb
lucas-mior <lucas.mior@tutamail.com>
2025-10-21 09:54:36 -0300

    define integers

--- fmtgen.h
+++ fmtgen.h
@@ -1,6 +1,27 @@
 #include <stdlib.h>
 #include <stddef.h>
-#include "util.c"
+#include <stdint.h>
+
+#ifndef INTEGERS
+#define INTEGERS
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+typedef unsigned long long ulonglong;
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef size_t usize;
+typedef ssize_t isize;
+#endif
 
 #ifndef STRUCT_FMT_DEF
     #define STRUCT_FMT_DEF
