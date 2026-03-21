a802e8b428188f95e5a883e1e0e56bbdafe0ca03
lucas-mior <lucas.mior@tutamail.com>
2026-03-21 19:44:45 -0300

    main.c compreensive tests

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <limits.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "assert.c"
 
 #define STRUCT_NAME NumberStruct
 #define STRUCT_FIELDS                                                          \
@@ -60,87 +63,114 @@
     X(EIGHT)        \
     X(SIXTEEN)      \
     X(THIRTY2)      \
-    X(SIXTY4)       \
-
+    X(SIXTY4)
 #include "xenums.c"
 
 int
 main(int argc, char **argv) {
-    NumberStruct mine = {
-        .ic = 'c',
-        .uc = 'd',
-        .is = SHRT_MAX,
-        .us = USHRT_MAX,
-        .ii = INT_MAX,
-        .ui = UINT_MAX,
-        .il = LONG_MAX,
-        .ul = ULONG_MAX,
-        .f = 0.5f,
-        .d = 0.5,
-        .ld = 0.5
-    };
-
-    SmallStruct small = {
-        .string = "superstring",
-        .number_struct = mine
-    };
-
-    BigStruct big = {
-        .l = 100,
-        .small_struct = &small
-    };
-
-    BigStruct *pbig = &big;
-
     (void)argc;
     (void)argv;
 
-    for (enum WeekDay day = WEEK_DAY_SUNDAY; day < WEEK_DAY_LAST; day += 1) {
-        char *s = WEEK_DAY_str(day);
-        if (strcmp(s, "Unknown value") != 0) {
-            printf("day[%u] = %s\n", day, s);
+    {
+        char *str_ptr;
+
+        /* Standard Enum Tests */
+        ASSERT_EQUAL(WEEK_DAY_str(WEEK_DAY_SUNDAY), "WEEK_DAY_SUNDAY");
+        ASSERT_EQUAL(WEEK_DAY_str(WEEK_DAY_MONDAY), "WEEK_DAY_MONDAY");
+        ASSERT_EQUAL(WEEK_DAY_str(WEEK_DAY_SATURDAY), "WEEK_DAY_SATURDAY");
+        ASSERT_EQUAL(WEEK_DAY_str(999), "Unknown value");
+
+        /* Bitflag Enum Tests */
+        if ((str_ptr = POWER_OF2_str(POWER_OF2_ONE))) {
+            ASSERT_EQUAL(str_ptr, "POWER_OF2_ONE");
+            free(str_ptr);
         }
-    }
 
-    for (enum PowerOfTwo x = POWER_OF2_ONE; x < POWER_OF2_LAST; x += 1) {
-        char *value_name = POWER_OF2_str(x);
-        printf("enum[%u] = %s\n", x, value_name);
-    }
+        if ((str_ptr = POWER_OF2_str(POWER_OF2_ONE | POWER_OF2_FOUR | POWER_OF2_SIXTY4))) {
+            ASSERT_EQUAL(str_ptr, "POWER_OF2_ONE|POWER_OF2_FOUR|POWER_OF2_SIXTY4");
+            free(str_ptr);
+        }
 
-    STRUCT_PRINT(&small);
-    STRUCT_PRINT(&mine);
-    STRUCT_PRINT(&big);
-    STRUCT_PRINT(pbig);
+        ASSERT_EQUAL(POWER_OF2_str(0), "NONE");
+    }
 
     {
-        SmallStruct small_from_buf;
-        NumberStruct num_from_buf;
-        uchar *s_buf;
-        uchar *n_buf;
+        ASSERT_EQUAL(NumberStruct_fmt.num_members, 11);
+        ASSERT_EQUAL(SmallStruct_fmt.num_members, 2);
+        ASSERT_EQUAL(BigStruct_fmt.num_members, 2);
 
-        if ((s_buf = malloc(SmallStruct_fmt.packed_size)) == NULL) {
-            return EXIT_FAILURE;
-        }
-        if ((n_buf = malloc(NumberStruct_fmt.packed_size)) == NULL) {
-            return EXIT_FAILURE;
-        }
+        ASSERT_EQUAL(NumberStruct_fmt.struct_name, "NumberStruct");
+        ASSERT_EQUAL(SmallStruct_fmt.names[0], "string");
+        ASSERT_EQUAL(SmallStruct_fmt.types[0], "char *");
 
-        SmallStruct_pack(&small, s_buf);
-        printf("SmallStruct packed:\n\t");
-        print_buffer(s_buf, SmallStruct_fmt.packed_size);
-        printf("\n");
-
-        NumberStruct_pack(&mine, n_buf);
-        printf("NumberStruct packed:\n\t");
-        print_buffer(n_buf, NumberStruct_fmt.packed_size);
-        printf("\n");
-
-        SmallStruct_unpack(s_buf, &small_from_buf);
-        NumberStruct_unpack(n_buf, &num_from_buf);
-
-        STRUCT_PRINT(&small_from_buf);
-        STRUCT_PRINT(&num_from_buf);
+        /* Check packed size against manual calculation */
+        size_t expected_small_size = sizeof(char *) + sizeof(NumberStruct);
+        ASSERT_EQUAL(SmallStruct_fmt.packed_size, expected_small_size);
     }
 
+    {
+        NumberStruct original_num = {
+            .ic = 'c',
+            .uc = 'd',
+            .is = SHRT_MAX,
+            .us = USHRT_MAX,
+            .ii = INT_MAX,
+            .ui = UINT_MAX,
+            .il = LONG_MAX,
+            .ul = ULONG_MAX,
+            .f = 0.5f,
+            .d = 0.5,
+            .ld = 0.5
+        };
+        NumberStruct restored_num;
+        SmallStruct small_val = {
+            .string = "reflection test",
+            .number_struct = original_num
+        };
+        SmallStruct restored_small;
+        BigStruct big_val = {
+            .l = 123456789,
+            .small_struct = &small_val
+        };
+        BigStruct *p_big = &big_val;
+        uchar *n_buf;
+        uchar *s_buf;
+
+        if ((n_buf = xmalloc(NumberStruct_fmt.packed_size)) == NULL) {
+            return EXIT_FAILURE;
+        }
+        NumberStruct_pack(&original_num, n_buf);
+        NumberStruct_unpack(n_buf, &restored_num);
+
+        ASSERT_EQUAL(original_num.ic, restored_num.ic);
+        ASSERT_EQUAL(original_num.is, restored_num.is);
+        ASSERT_EQUAL(original_num.ui, restored_num.ui);
+        ASSERT_EQUAL(original_num.ld, restored_num.ld);
+
+        if ((s_buf = xmalloc(SmallStruct_fmt.packed_size)) == NULL) {
+            free(n_buf);
+            return EXIT_FAILURE;
+        }
+        SmallStruct_pack(&small_val, s_buf);
+        SmallStruct_unpack(s_buf, &restored_small);
+
+        ASSERT_EQUAL(small_val.string, restored_small.string);
+        ASSERT_EQUAL(small_val.number_struct.ii, restored_small.number_struct.ii);
+        ASSERT_EQUAL(small_val.number_struct.f, restored_small.number_struct.f);
+
+        printf("--- Printing BigStruct (Includes Pointer to SmallStruct) ---\n");
+        STRUCT_PRINT(&big_val);
+
+        printf("--- Printing BigStruct via Pointer ---\n");
+        STRUCT_PRINT(p_big);
+
+        printf("--- Printing Restored NumberStruct ---\n");
+        STRUCT_PRINT(&restored_num);
+
+        free(n_buf);
+        free(s_buf);
+    }
+
+    printf("\nmain.c: All comprehensive tests passed.\n");
     return EXIT_SUCCESS;
 }
