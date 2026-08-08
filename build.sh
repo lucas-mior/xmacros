6f68b07ac9fa76c5764644e60db1f432999ba0e0
lucas-mior <lucas.mior@tutamail.com>
2026-08-08 11:18:23 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -69,7 +69,6 @@ if [ "$CC" = "clang" ]; then
     CFLAGS="$CFLAGS -Wno-used-but-marked-unused"
     CFLAGS="$CFLAGS -Wno-implicit-int-enum-cast"
     CFLAGS="$CFLAGS -Wno-assign-enum"
-    CFLAGS="$CFLAGS -Wno-cast-function-type-strict"
     CFLAGS="$CFLAGS -Wno-bad-function-cast"
 
     CFLAGS="$CFLAGS -Wno-double-promotion"
